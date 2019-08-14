1a12730fd8a05bf0c681cfaa57bbc3567bd45a34
Raymond Yang <raymondyang@outlook.co.nz>
2019-08-14 22:05:12 +1200

    Added functionality

--- wiki_speak.sh
+++ wiki_speak.sh
@@ -39,26 +39,73 @@ main_menu (){
 }
 
 list_creations (){
-	echo "Listing was called"
+	if [[ -z "`ls -A ./creations`" ]]
+	then
+		echo "No creations found. "
+	else
+		counter=1
+		ls ./creations | sed 's/.mp4/\n/g' | sed '/^$/d' | sort | while read item ; do
+			echo -n "[$counter]" 
+			echo " $item"
+			counter=$((counter+1))
+		done
+	fi
+	
 }
 
 play_creations (){
-	echo "playing was called"
+	list_creations
+	while [[ true ]]
+	do		
+		read -p "Which creation would you like to play? Enter the name (not the number) - " play_choice
+		if [[ -f ./creations/"$play_choice.mp4" ]]
+		then
+			break
+		else
+			echo "That creation does not exist."
+		fi
+	done
+	ffplay -autoexit ./creations/"$play_choice.mp4" &> /dev/null
+	if [[ $? != 0 ]]
+	then
+		echo "Creation could not be played."
+		echo ""
+		read -n 1 -s -r -p "Press any key to return to main menu  "
+		echo ""
+		main_menu
+	fi
+	main_menu
 }
 
 delete_creations (){
-	echo "deleting was called"
+	list_creations
+	while [[ true ]]
+	do		
+		read -p "Which creation would you like to delete? Enter the name (not the number) - " delete_choice
+		if [[ -f ./creations/"$delete_choice.mp4" ]]
+		then
+			read -p "You are going to delete $delete_choice. Are you sure [y/n] - " delete_sure
+			if [[ $delete_sure == "y" ]]
+			then
+				rm -f ./creations/"$delete_choice.mp4"
+				echo "Delete Successful!"
+				break
+			fi
+		else
+			echo "That creation does not exist."
+		fi
+	done
+	main_menu
 }
 
 create_creations (){
-	echo "creating was called"
 	get_wiki_search
 	
 	#display sentences to terminal
 	counter=1
-	echo $wiki_search | sed 's/[.]/&\n/g' | sed '$d' | while read sentence ; do
+	echo $wiki_search | sed 's/[.]/&\n/g' | sed '/^$/d' | while read sentence ; do
 		echo -n "[$counter]" 
-		echo $sentence | tee -a temp.txt
+		echo $sentence | tee -a temp.txt	
 		counter=$((counter+1))
 	done 
 	
@@ -87,23 +134,50 @@ create_creations (){
 	while [[ true ]]
 	do
 		read -p "Enter a name for this creation - " creation_name
-		if [[ -f $creation_name ]]
+		if [[ -f ./creations/"$creation_name.mp4" ]]
 		then
 			echo "File already exists."
+			read -p "Would you like to overwrite? [y/n] " do_overwrite
+			if [[ $do_overwrite == "y" ]]
+			then
+				rm -f ./creations/"$creation_name.mp4"
+				break
+			fi
 		else
 			break
 		fi
 	done
 	
+	echo -n "Creating $creation_name. Please wait...  "
+	
+	#create the creation
 	vid_length=`soxi -D speech.wav`
-	ffmpeg -f lavfi -i color=c=blue:s=320x240 -t "${vid_length}" -vf "drawtext=fontsize=30:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$searched_word'" video.mp4
+	ffmpeg -f lavfi -i color=c=blue:s=320x240 -t "${vid_length}" -vf "drawtext=fontsize=30:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='$searched_word'" video.mp4 &> /dev/null
+	if [[ $? == 0 ]]
+	then
+		ffmpeg -i video.mp4 -i speech.wav ./creations/"${creation_name}".mp4 &> /dev/null
+		if [[ $? == 0 ]]
+		then
+			echo "Success!"
+		else
+			echo "Failed!" >&2
+		fi
+	else 
+		echo "Failed!" >&2
+	fi
+
 	
-	mkdir -p creations
-	ffmpeg -i video.mp4 -i speech.wav ./creations/"${creation_name}".mp4 
 	
+	#remove temporary files
 	rm -f temp.txt
 	rm -f speech.wav
 	rm -f video.mp4
+	
+	echo ""
+	read -n 1 -s -r -p "Press any key to return to main menu  "
+	echo ""
+	
+	main_menu
 }
 
 get_wiki_search (){
@@ -126,4 +200,6 @@ get_wiki_search (){
 	done
 }
 
+#make the subfolder for creations
+mkdir -p creations
 main_menu
